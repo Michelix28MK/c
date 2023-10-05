db025545eff7d287ea81bbd8cd5cb5a8dc4c668c
Michelix28MK <michele.folci04@gmail.com>
2023-10-06 00:06:01 +0200

    problema di gaus
    Risoluzione del problema di Gaus tramite un algoritmo in c. (esercizio in classe)

---
+++ somma_dei_mille.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sommatoria = 0;
+int val_sommato = 0;
+
+int main()
+{
+    do
+    {
+        sommatoria += val_sommato;
+        //printf("%d\n", val_sommato); chek dei numeri sommati
+        val_sommato++;
+    } while (val_sommato <= 1000);
+    printf("%s", "La somma dei primi numeri naturali da 1 a 1000 è: ");
+    printf("%d\n",sommatoria);
+    
+    return 0;
+}
