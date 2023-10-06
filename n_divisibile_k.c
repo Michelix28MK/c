6216fbf5aacb134254a4d9e58c70db373cba7d69
Michelix28MK <michele.folci04@gmail.com>
2023-10-06 16:07:24 +0200

    esercizio 2 lezione 3

---
+++ n_divisibile_k.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int n, k;
+
+int main(){
+    do
+    {
+        printf("%s", "inserire il valore di N che sia maggiore di 0: ");
+        scanf("%d", &n);
+
+        if (n <= 0)
+            printf("%s", "\n\nil valore non è maggiore di 0");
+    } while (n <= 0);
+    do
+    {
+        printf("%s", "inserire il valore di K che sia maggiore di 0: ");
+        scanf("%d", &k);
+
+        if (k <= 0)
+            printf("%s", "\n\nil valore non è maggiore di 0");
+    } while (k <= 0);
+
+    if (n%k == 0)
+    {
+        printf("%s\n", "N è divisibile per K ");
+    }
+    else{
+        printf("%s\n", "N non è divisibile per K");
+    }
+    return 0;
+}
