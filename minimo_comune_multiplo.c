059a9da82746a910cf5107cd756b6401f1590049
Michelix28MK <michele.folci04@gmail.com>
2023-10-06 17:17:01 +0200

    esercizio 3 lezione 3

---
+++ minimo_comune_multiplo.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int n, k, c;
+
+int main(){
+    do
+    {
+        printf("%s", "inserire il valore di N che sia maggiore di 0: ");
+        scanf("%d", &n);
+
+        if (n <= 0)
+            printf("%s", "\n\nil valore non è maggiore di 0");
+    } while (n <= 0);
+    do
+    {
+        printf("%s", "inserire il valore di K che sia maggiore di 0: ");
+        scanf("%d", &k);
+
+        if (k <= 0)
+            printf("%s", "\n\nil valore non è maggiore di 0");
+    } while (k <= 0);
+    //sorting
+    if (n<k)
+    {
+        c=n;
+        n=k;
+        k=c;
+    }
+    //sorting test done
+    
+    //parto dal più grande
+    //controllo la divisibilità reciproca
+    //sommo sommo n a sestesso.
+    if (n%k == 0)
+    {
+        printf("%s", "\n Il minimo comune multiplo e': ");
+        printf("%d", n);
+    } else {
+        //imposto la partenza del processo
+        c = n;
+
+        do
+        {
+            c += n;
+
+        } while (c%k != 0);
+        
+        printf("%s", "\n Il minimo comune multiplo e': ");
+        printf("%d", c);
+    }
+    return 0;
+}
