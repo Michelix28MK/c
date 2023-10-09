b63f906f5e8313bb85df71e330babdf281fba84a
Michelix28MK <michele.folci04@gmail.com>
2023-10-09 21:18:56 +0200

    quasi terminati gli esercizi di laboratorio

--- esercizi_laboratorio_1.c
+++ esercizi_laboratorio_1.c
@@ -3,14 +3,35 @@
 #include <limits.h>
 #include <float.h>
 
+int a = INT_MAX, b[2];
+float num = FLT_MIN;
+
 int main (){
 
     printf("Esercizio1: scrivere un programma che stampi:\nHello word\n");
     printf("\nEsercizio2: scrivere un programma che stampi: \n Hellow,\n word\n");
     printf("\nEsercizio3: Scrivi un programma che tabulizza il testo: \n\tdato1 \tdato2 \n\tdato3 \tdato4\n");
-    printf("Esercizio4: stampa la scritta Ciao tramite codici Hashii: \103 \111 \101 \117");
-    printf("Esercizio5: Stamnpa i limit di variabile di:\n\tcarattere (%d | %d) \tinteri(%d | %d)\n", CHAR_MIN, CHAR_MAX, INT_MIN, INT_MAX);
-    printf("Esercizio5: Stamnpa i limit di variabile di:\n\tfloat (%g | %g) \tduble(%g | %g)", FLT_MIN, FLT_MAX, DBL_MIN, DBL_MAX);
-    //esercizi dafinire fino al 11
+    printf("\nEsercizio4: stampa la scritta Ciao tramite codici Hashii: \103 \111 \101 \117"); //codice octa ascii
+    printf("\nEsercizio5: Stamnpa i limit di variabile di:\n\tcarattere (%d | %d) \tinteri(%d | %d)\n", CHAR_MIN, CHAR_MAX, INT_MIN, INT_MAX);
+    printf("\nEsercizio6: Stamnpa i limit di variabile di:\n\tfloat (%g | %g) \tduble(%g | %g)", FLT_MIN, FLT_MAX, DBL_MIN, DBL_MAX);
+
+    a++;
+    printf("\nEsercizio7: assegna 'INT_MAX' a una varaiabile e incremente di uno, il risultato è %d", a);
+
+    num = num/FLT_MAX;
+
+    printf("\nEsercizio8: il valore di 'FLT_MIN'/'FLT_MAX' e\' : %f", num);
+    printf("\nEsercizio9: inserisci un valore numerico ce verrà stampato: ");
+    scanf("%d", &a);
+    printf("il numero e\' %d", a);
+    printf("\nEsercizio10: inserisci 2 valori di cui si restituirà delle operazioni:\n ");
+
+    for (int i = 0; i < 2; i++)
+    {   
+        printf("\nInserire il %d valore: ", i+1);
+        scanf("%d", &b[i]);
+    }
+    printf("\n\tsomma: %d \tsottrazione: %d \tprodotto: %d \trapporto: %d \tmodulo: %d", (b[1]+b[2]), (b[1]-b[2]), (b[1]*b[2]), (b[1]/b[2]), (b[1]%b[2]));
+     //testare a pezzi per identificare il problema
     return 0;
 }
