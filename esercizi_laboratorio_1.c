b3849fe9548991048dada54cd445f9858eda2e50
Michelix28MK <michele.folci04@gmail.com>
2023-10-09 12:09:58 +0200

    esercizi dal 1 al 5 di laboratorio

---
+++ esercizi_laboratorio_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+int main (){
+
+    printf("Esercizio1: scrivere un programma che stampi:\nHello word\n");
+    printf("\nEsercizio2: scrivere un programma che stampi: \n Hellow,\n word\n");
+    printf("\nEsercizio3: Scrivi un programma che tabulizza il testo: \n\tdato1 \tdato2 \n\tdato3 \tdato4\n");
+    printf("Esercizio4: stampa la scritta Ciao tramite codici Hashii: \103 \111 \101 \117");
+    printf("Esercizio5: Stamnpa i limit di variabile di:\n\tcarattere (%d | %d) \tinteri(%d | %d)\n", CHAR_MIN, CHAR_MAX, INT_MIN, INT_MAX);
+    printf("Esercizio5: Stamnpa i limit di variabile di:\n\tfloat (%g | %g) \tduble(%g | %g)", FLT_MIN, FLT_MAX, DBL_MIN, DBL_MAX);
+    //esercizi dafinire fino al 11
+    return 0;
+}
--- helloword.c
+++ helloword.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h> //definizione dei limiti di variabili int e  char
+#include <float.h> //definizione dei limiti di variabili float e duble
 
 int main (){
 
