e324cd7005999c09600250962d8f9e1d27c56a59
Michelix28MK <michele.folci04@gmail.com>
2023-10-12 14:27:09 +0200

    ricostruzione dell'esercizio 6 con i caratteri (5 - 6 byte)

--- eliminazione_doppi.c
+++ eliminazione_doppi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define UNO (n/100000000)%10
 #define DUE (n/10000000)%10
@@ -9,42 +10,45 @@
 #define SETTE (n/100)%10
 #define OTTO (n/10)%10
 #define NOVE n%10
-#define EX printf("valore: %d \t controllo: %d\n", letto, n);
+#define EX printf("\n%c \t%d\n", letto, n);
 
 int main() {
-    int letto, n;
+    unsigned int n;
+    unsigned char letto;
+
     do
-    {
-        printf("inserisci il numero: ");
-        scanf("%d", &letto);
+    {   
+        printf("inserisci la cifra: ");
+        letto = getchar();
+        getchar();//scarico dell'invio
         printf("\033[A"); // Sposta il cursore su una riga sopra - codice di escape, che indica che seguiranno dei comandi di controllo ANSI.
         printf("\033[K"); //sovrascrive l'interariga su cui si trova il cursore
+        
         switch (letto)
         {   
-            case 1:
+            case '1':
                 if (UNO != 1){n += 100000000; EX} break;
-            case 2:
+            case '2':
                 if (DUE != 2){n += 20000000; EX} break;
-            case 3:
+            case '3':
                 if (TRE != 3){n += 3000000; EX} break;
-            case 4:
+            case '4':
                 if (QUATTRO != 4){n += 400000; EX} break;
-            case 5:
+            case '5':
                 if (CINQUE != 5){n += 50000; EX} break;
-            case 6:
+            case '6':
                 if (SEI != 6){n += 6000; EX} break;
-            case 7:
+            case '7':
                 if (SETTE != 7){n += 700; EX} break;
-            case 8:
+            case '8':
                 if (OTTO != 8){n += 80; EX} break;
-            case 9:
+            case '9':
                 if (NOVE != 9){n += 9; EX} break;
-            case 0:
+            case '0':
                 printf("\nla sequenza e\' finita!"); break;
             default:
-                printf("\nError: \tCarattere inserito non e\' accettabile! -> ");
-            break;
+                printf("Error: \tCarattere inserito non valido! -->"); break;
         }
-    } while (letto != 0);
+    } while (letto != '0');
     return 0;
 }
