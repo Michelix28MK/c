78a3a418b017c3faad78dcff89f13601878aedc1
Michelix28MK <michele.folci04@gmail.com>
2023-11-10 12:06:33 +0100

    comportamento del pedone completato! (da testare)

--- progetti/gioco schacci.c
+++ progetti/gioco schacci.c
@@ -12,13 +12,13 @@ int main (){
 
     int tavola[8][8], mn[5], turno = 0;
 
-    void set_tavola(int tavola[][DIM]); //utilizzabile per reset e set iniziale
+    void set_tavola(); //utilizzabile per reset e set iniziale
 
-    char trova_pedina(int tavola[][DIM], int numeri, int lettere);
+    char trova_pedina();
 
-    void show_tavola(int tavola[][DIM]); // mostra a schermo la situzione sulla tavola
+    void show_tavola(); // mostra a schermo la situzione sulla tavola
 
-    void next_move(int tavola[][DIM], int *turno);
+    void next_move();
 
     void move_pedone();
     void move_re();
@@ -26,7 +26,6 @@ int main (){
     void move_torre();
     void move_cavallo();
     void move_alfiere();
-    void clear_cella();
 
     set_tavola(tavola);
     show_tavola(tavola);
@@ -51,7 +50,7 @@ void set_tavola(int tavola[][DIM]){
         tavola[7][i]= (i+17); //imposta famiglai reale 2
         tavola[6][i]= (i+25); //imposta pedoni 2
     }
-    printf("la tavola e\' stata impostata per una nuova partita:\n ");
+    printf("la tavola e\' stata impostata per una nuova partita:\n\n il giocatore 1 ha le pedine maiuscole (QK)\n");
 }
 
 char trova_pedina(int tavola[][DIM], int numeri, int lettere){
@@ -155,6 +154,15 @@ void next_move(int tavola[][DIM], int *numero_giocatore){
         scanf("%c", &conferma);
         getchar();
 
+        if (((*numero_giocatore == 0) && (tavola[numeri[0]-1][numeri[2]>16])) ||
+            ((*numero_giocatore == 1) && (tavola[numeri[0]-1][numeri[2]<16]))
+            )
+        {   
+            printf("\nError: La pedina selezionata non è ti appartiene!\n\n");
+            conferma = 'n';
+        }
+        
+
     }while (conferma != 'y');
 
     switch (pedina)
@@ -194,32 +202,68 @@ void move_pedone(int tavola[][DIM], int posY, int posX, int utente, int pos1Y, i
     */
     if (utente)
     {       // pedine a disposizione dalla 25 alla 32
-        
+        int primo_spostamento_Y = (posY == 6);
+        int spostamento_Y_doppio_valido = (pos1Y - posY < 0) && (pos1Y - posY > -3);
+        int spostamento_Y_singolo_valido = (pos1Y - posY < 0) && (pos1Y - posY > -2); //spostamento in avanti standard
+        int spostamento_X_valido = (pos1X - posX < 2) && (pos1X - posX > -2);//spostamento laterale da -1 e +1
+        int spostamento_X_nullo = (pos1X - posX == 0);
+        int cella_frontale_libera = !tavola[posY-1][posX];
+        int due_cella_frontali_libere = (!tavola[posY-2][pos1X]) && cella_frontale_libera; //se libera restituisce 1 se occupata restituisce 0| sistemare il metodo di controllo
+        int nemico_nella_cella_DX = (tavola[posY-1][posX+1] > 16);
+        int nemico_nella_cella_SX = (tavola[posY-1][posX-1] > 16);
+
+        if ((primo_spostamento_Y && spostamento_Y_doppio_valido && due_cella_frontali_libere && spostamento_X_nullo)/*avanzamento doppio*/ ||
+            (spostamento_Y_singolo_valido && cella_frontale_libera && spostamento_X_nullo)/*avanzamento dingolo*/ ||
+            (spostamento_Y_singolo_valido && !spostamento_X_nullo && spostamento_X_valido && nemico_nella_cella_DX) /*mangia DX*/ ||
+            (spostamento_Y_singolo_valido && !spostamento_X_nullo && spostamento_X_valido && nemico_nella_cella_DX) /*mangia SX*/)
+        {
+            tavola[pos1X][pos1Y] = tavola[posX][posY];
+            tavola[posX][posY] = 0;
+            utente = !utente;
+        }else{
+            printf("Error: Mossa non valida! ripetere il turno.");
+        }
     }else{ //pedine a disposizione dalla 9 alla 16 alla 
         //determino se la posizione davanti è libera
         int primo_spostamento_Y = (posY == 1);
+        int spostamento_Y_doppio_valido = (pos1Y - posY > 0) && (pos1Y - posY < 3);
         int spostamento_Y_singolo_valido = (pos1Y - posY > 0) && (pos1Y - posY < 2); //spostamento in avanti standard
         int spostamento_X_valido = (pos1X - posX < 2) && (pos1X - posX > -2);//spostamento laterale da -1 e +1
-        int spostamento_X_nullo = (pos1Y - posY == 0);
-        int cella_frontale_libera = !tavola[pos1Y][pos1X]; //se libera restituisce 1 se occupata restituisce 0| sistemare il metodo di controllo
-        int cella_DX_libera = !tavola[pos1Y][posX+1];
-        int cella_SX_libera = !tavola[pos1Y][posX-1];
+        int spostamento_X_nullo = (pos1X - posX == 0);
+        int cella_frontale_libera = !tavola[posY+1][posX];
+        int due_cella_frontali_libere = (!tavola[posY+2][pos1X]) && cella_frontale_libera; //se libera restituisce 1 se occupata restituisce 0| sistemare il metodo di controllo
+        int nemico_nella_cella_DX = (tavola[posY+1][posX+1] > 16);
+        int nemico_nella_cella_SX = (tavola[posY+1][posX-1] > 16);
 
-        if (spostamento_Y_singolo_valido && cella_frontale_libera && spostamento_X_nullo)
-        {   //il pedone avanza
+        if ((primo_spostamento_Y && spostamento_Y_doppio_valido && due_cella_frontali_libere && spostamento_X_nullo)/*avanzamento doppio*/ ||
+            (spostamento_Y_singolo_valido && cella_frontale_libera && spostamento_X_nullo)/*avanzamento dingolo*/ ||
+            (spostamento_Y_singolo_valido && !spostamento_X_nullo && spostamento_X_valido && nemico_nella_cella_DX) /*mangia DX*/ ||
+            (spostamento_Y_singolo_valido && !spostamento_X_nullo && spostamento_X_valido && nemico_nella_cella_DX) /*mangia SX*/)
+        {
             tavola[pos1X][pos1Y] = tavola[posX][posY];
             tavola[posX][posY] = 0;
-        }else if (spostamento_Y_singolo_valido && !spostamento_X_nullo && spostamento_X_valido)
-        {
-            /* code */
+            utente = !utente;
+        }else{
+            printf("Error: Mossa non valida! ripetere il turno.");
         }
-        
-        
+
     }
+    int fine_tavola = (pos1Y == 7) && (pos1Y == posY + 1) || (pos1Y == 0) && (pos1Y == posY - 1);
+    if (fine_tavola) //se il pedone selezionato riesce ad arrivare alla fine allora gli si fascegliere una figura con cui sostituirlo
+    {
+        printf(" con cosa vuoi sotituire il tuo pedone?"); //da sviluppare
+    } 
     
 }
-void move_re(int tavola[][DIM], int posX, int posY, int utente, int pos1X, int pos1Y){
 
+void move_re(int tavola[][DIM], int posX, int posY, int utente, int pos1X, int pos1Y){
+    if (utente)
+    {
+        
+    }else{
+
+    }
+    
 }
 void move_regina(int tavola[][DIM], int posX, int posY, int utente, int pos1X, int pos1Y){
 
