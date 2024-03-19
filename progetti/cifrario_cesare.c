293bf2177a9d7b62bdb17fa1b640883276592c2b
Michelix28MK <michele.folci04@gmail.com>
2024-03-19 14:55:15 +0100

    Gioco degli scacchi terminato

--- progetti/cifrario_cesare.c
+++ progetti/cifrario_cesare.c
@@ -16,24 +16,30 @@ void cifra(char *stringa, int k)
 {
     for (int i = 0; i < strlen(stringa); i++)
     {
-        printf("\n%c + %d =>", stringa[i], k);
-        if ((stringa[i] + k) > z)
-            stringa[i] += (k - 25);
-        else
-            stringa[i] += k;
-        printf("%c", stringa[i]);
+        if (!((int)stringa[i]<a) || ((int)stringa[i]>z)) //solo caratteri minuscoli
+        {
+            //printf("\n%c + %d =>", stringa[i], k);
+            if ((stringa[i] + k) > z)
+                stringa[i] += (k - 25);
+            else
+                stringa[i] += k;
+            //printf("%c", stringa[i]);
+        }
     }
 }
 void decifra(char *stringa, int k)
 {
     for (int i = 0; i < strlen(stringa); i++)
     {
-        printf("\n%c - %d =>", stringa[i], k);
-        if ((stringa[i] - k) < a)
-            stringa[i] -= (k + 25);
-        else
-            stringa[i] -= k;
-        printf("%c", stringa[i]);
+        if (!((int)stringa[i]<a) || ((int)stringa[i]>z))
+        {
+            //printf("\n%c - %d =>", stringa[i], k);
+            if ((stringa[i] - k) < a)
+                stringa[i] -= (k + 25);
+            else
+                stringa[i] -= k;
+            //printf("%c", stringa[i]);
+        }
     }
 }
 struct riga
@@ -66,13 +72,13 @@ int main(void)
     struct riga file_letto;
     file_letto.numero_puntatori = 0;
 
-    FILE *ptr, *ptr2;
-    char titolo[BUFSIZ];
+    FILE *ptr;
+    char titolo[BUFSIZ], save;
     int shift, fine_file, funzionalita;
     //funzionalità
     printf("\n 1 - cifra \n 2 - decifra \n\n scelta: ");
     scanf("%d", &funzionalita);
-    gets();
+    getchar();
     //selezione file
     printf("Inserisci il nome del file da cifrare: ");
     fgets(titolo, BUFSIZ, stdin);
@@ -86,6 +92,7 @@ int main(void)
     
     printf("Fattore di spostamento: ");
     scanf("%d", &shift);
+    getchar();
     //leggi file
     fine_file = LeggiRiga(&file_letto, ptr);
     while (fine_file)
@@ -97,8 +104,7 @@ int main(void)
         for (size_t i = 0; i < file_letto.numero_puntatori; i++)
         {
             cifra(file_letto.caratteri[i], shift);
-        }
-        
+        }   
     }
     else
     {
@@ -107,11 +113,30 @@ int main(void)
             decifra(file_letto.caratteri[i], shift);
         }
     }
+    fclose(ptr);
     //stampa file
+    printf("\n\nIl file è stato cifrato: ");
     for(int i = 0; file_letto.numero_puntatori > i; i++)
         printf("\n%s", file_letto.caratteri[i]);
-    //libero malloc    
-    free(file_letto.caratteri);
-    
+    //libero malloc
+    printf("\n\nSalvare il testo in un nuovo file (y/n): ");
+    scanf("%c", &save);
+    getchar();
+
+    if (save == 'y')
+    {   //titolo
+        printf("\nInserisci il titolo.txt: ");
+        fgets(titolo, BUFSIZ, stdin);
+        //apri file
+        set_string(titolo);
+        ptr = fopen(titolo, "w");
+        rewind(ptr);
+
+        for (size_t i = 0; i < file_letto.numero_puntatori; i++)
+        {
+            fprintf(ptr, "%s", file_letto.caratteri[i]);
+        }
+        printf("\nFine copiatura");
+    }
     return 0;
 }
